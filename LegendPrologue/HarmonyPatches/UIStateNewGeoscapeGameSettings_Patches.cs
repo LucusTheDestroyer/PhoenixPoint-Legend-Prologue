7d381072ee89f4105b5e927b9860ff7ef6ba9fb1
Luc <lucoejames@hotmail.co.uk>
2024-09-10 14:32:44 +0100

    Adds harmony patch to remove secondary safeguard which prevents the Prologue on Legend

---
+++ LegendPrologue/HarmonyPatches/UIStateNewGeoscapeGameSettings_Patches.cs
@@ -0,0 +1,34 @@
+using HarmonyLib;
+using PhoenixPoint.Home.View.ViewStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+
+namespace LegendPrologue.Harmony 
+{
+    [HarmonyPatch(typeof(UIStateNewGeoscapeGameSettings), "GameSettings_OnConfirm", new Type[]{})]
+    public static class LegendByDefault_Patch2
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> listInstructions = new List<CodeInstruction>(instructions);
+            IEnumerable<CodeInstruction> insert = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Stloc_S, 6),
+            };
+
+            for (int i=0; i < instructions.Count(); i++)
+            {
+                if(listInstructions[i].opcode == OpCodes.Stloc_S && listInstructions[i].operand is LocalBuilder local && local.LocalIndex == 6)
+                {
+                    listInstructions.InsertRange(i+1, insert);
+                    return listInstructions;
+                }
+            }
+            return instructions;
+        }
+    }
+}
