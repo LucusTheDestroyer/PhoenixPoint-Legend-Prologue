31297dff41d026be562e28e1f5e2b900f6ff9cfb
Luc <lucoejames@hotmail.co.uk>
2024-09-10 14:48:34 +0100

    Was being picky, but removed a blank line...

--- LegendPrologue/HarmonyPatches/UIStateNewGeoscapeGameSettings_Patches.cs
+++ LegendPrologue/HarmonyPatches/UIStateNewGeoscapeGameSettings_Patches.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 
-
 namespace LegendPrologue.Harmony 
 {
     [HarmonyPatch(typeof(UIStateNewGeoscapeGameSettings), "GameSettings_OnConfirm", new Type[]{})]
